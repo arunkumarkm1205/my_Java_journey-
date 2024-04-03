1b5a6a2b2be561165cea5d6a52fe091b8b587f41
Arun Kumar K M <arunkumarkm1205@gmail.com>
2024-04-03 22:20:16 +0530

    Wrapper

---
+++ Wrapperclass/src/wrapperclass/Wrapperclass.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wrapperclass;
+
+/**
+ *
+ * @author arunk
+ */
+public class Wrapperclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Integer k=10;
+        int m=20;
+        Integer h=m; // auto boxing 
+        int l=k; // auto unboxing 
+        
+        Float f=12.f;
+        float ff=f;
+        
+    }
+    
+}
